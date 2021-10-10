fe51d1ab45ac0577c93731118a814291437ac353
Gabriele Canepa <git@gabrielecanepa.com>
2021-10-10 16:42:28 +0200

    Pass all test in TypeScript exercise grade-school

--- typescript/grade-school/grade-school.ts
+++ typescript/grade-school/grade-school.ts
@@ -3,44 +3,44 @@ interface Roster {
 }
 
 export class GradeSchool {
-  private grades: Roster
+  private _roster: Roster
 
   constructor() {
-    this.grades = {}
+    this._roster = {}
   }
 
   public roster(): Roster {
-    return this.grades
+    return JSON.parse(JSON.stringify(this._roster))
   }
 
   public grade(grade: number): string[] {
-    return [...(this.grades[grade] || [])]
+    return this.roster()[grade] ?? []
   }
 
   public add(student: string, grade: number): void {
     const studentGrade = this.getStudentGrade(student)
     if (studentGrade) {
       if (studentGrade === grade) return
-      this.grades[studentGrade] = this.grades[studentGrade].filter(student => student !== student)
+      this._roster[studentGrade] = this._roster[studentGrade].filter(student => student !== student)
     }
-    const studentsInGrade = this.grades[grade]
+    const studentsInGrade = this._roster[grade]
     if (studentsInGrade) {
       studentsInGrade.push(student)
     } else {
-      this.grades[grade] = [student]
+      this._roster[grade] = [student]
     }
     this.sort()
   }
 
   private sort(): void {
-    this.grades = Object.keys(this.grades).sort().reduce((grades, grade) => {
-      (grades as any)[grade] = (this.grades as any)[grade].sort()
+    this._roster = Object.keys(this._roster).sort().reduce((grades, grade) => {
+      (grades as any)[grade] = (this._roster as any)[grade].sort()
       return grades
     }, {})
   }
 
   private getStudentGrade(student: string): number | null {
-    const grade = Object.keys(this.grades).find(grade => (this.grades as any)[grade].includes(student))
+    const grade = Object.keys(this._roster).find(grade => (this._roster as any)[grade].includes(student))
     return grade ? parseInt(grade) : null
   }
 }
